b63c9582d8c97db3e77638a805eec9276ef22d9e
Denis <omelched@gmail.com>
2022-07-11 17:40:00 +0300

    fixes

--- frontol/urls.py
+++ frontol/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from .views import *
 
 urlpatterns = (
-    path("document",    DocumentAPIView.as_view()),
-    path("client",      ClientAPIView.as_view())
+    path("document/",    DocumentAPIView.as_view()),
+    path("client/",      ClientAPIView.as_view())
 )
